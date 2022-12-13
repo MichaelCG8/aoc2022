37febc6167db6c36f7804767b53c26e807ff25a9
Michael Garbutt <michael.c.garbutt@gmail.com>
2022-12-13 09:13:55 +0000

    Linting day 11

--- 11/lib.h
+++ 11/lib.h
@@ -10,28 +10,45 @@
 
 #include "../aoc.h"
 
+#define N_ROUNDS_1 (20)
+#define N_ROUNDS_2 (10'000)
+#define WORRY_DIVISOR (3)
+
+
+enum LINE_INDEXES {
+    LINE_MONKEY_ID_IDX = 0,
+    LINE_STARTING_LEVELS_IDX = 2,
+    LINE_OPERATION_IDX = 2,
+    LINE_TEST_IDX = 2,
+    LINE_TRUE_DEST_IDX = 7,
+
+    MONKEY_ID_IDX = 7,
+    ITEMS_START = 18,
+    OPERATION_TYPE_IDX = 23,
+    OPERATION_VAL_START = 25,
+    TEST_DIVISOR_START = 21,
+    TRUE_DEST_IDX = 29,
+    FALSE_DEST_IDX = 30,
+};
+
 
 class Monkey {
     public:
         std::vector<uint64_t> worry_levels;
-        size_t true_destination;
-        size_t false_destination;
+        size_t true_destination {0};
+        size_t false_destination {0};
         std::function<uint64_t(uint64_t)> operation;
         std::function<bool(uint64_t)> test;
-        uint64_t n_inspections;
+        uint64_t n_inspections {0};
         
         Monkey()
-            : worry_levels(std::vector<uint64_t>())
-            , true_destination(0)
-            , false_destination(0)
-            , operation([](uint64_t worry_level) { return worry_level; })
-            , test([](uint64_t worry_level) { return true; })
-            , n_inspections(0)
+            : operation([](uint64_t worry_level) { return worry_level; })
+            , test([](uint64_t worry_level) { return true; })  // NOLINT(misc-unused-parameters)
             {}
 };
 
 template<size_t N_MONKEYS>
-int part1(const char* path) {
+uint64_t part1(const char* path) {
     std::string line;
     std::ifstream infile(path);
     if(!infile.is_open()) {
@@ -48,25 +65,25 @@ int part1(const char* path) {
         if(line.length() == 0) {
             continue;
         }
-        if(line[0] == 'M') {
-            monkey_id = line[7] - '0';
-        } else if(line[2] == 'S') {
-            auto items_string = line.substr(18);
+        if(line[LINE_MONKEY_ID_IDX] == 'M') {
+            monkey_id = line[MONKEY_ID_IDX] - '0';
+        } else if(line[LINE_STARTING_LEVELS_IDX] == 'S') {
+            auto items_string = line.substr(ITEMS_START);
             uint64_t worry_level = 0;
-            for(auto c : items_string) {
+            for(auto c : items_string) {  // NOLINT(readability-identifier-length)
                 if(c == ',') {
                     monkeys[monkey_id].worry_levels.push_back(worry_level);
                     worry_level = 0;
                 } else if(c == ' ') {
                     continue;
                 } else {
-                    worry_level = worry_level * 10 + c - '0';
+                    worry_level = worry_level * 10 + c - '0';  // NOLINT(*magic-numbers)
                 }
             }
             monkeys[monkey_id].worry_levels.push_back(worry_level);
-        } else if(line[2] == 'O') {
-            auto operation_type = line[23];
-            auto operation_value_string = line.substr(25);
+        } else if(line[LINE_OPERATION_IDX] == 'O') {
+            auto operation_type = line[OPERATION_TYPE_IDX];
+            auto operation_value_string = line.substr(OPERATION_VAL_START);
             if(operation_value_string[0] == 'o') {
                 if(operation_type == '*') {
                     monkeys[monkey_id].operation = std::function {
@@ -83,8 +100,8 @@ int part1(const char* path) {
                 }
             } else {
                 uint64_t operation_value = 0;
-                for(auto c : operation_value_string) {
-                    operation_value = operation_value * 10 + c - '0';
+                for(auto c : operation_value_string) {  // NOLINT(readability-identifier-length)
+                    operation_value = operation_value * 10 + c - '0';  // NOLINT(*magic-numbers)
                 }
                 if(operation_type == '*') {
                     monkeys[monkey_id].operation = std::function {
@@ -100,29 +117,29 @@ int part1(const char* path) {
                     };
                 }
             }
-        } else if(line[2] == 'T') {
-            auto divisor_string = line.substr(21);
+        } else if(line[LINE_TEST_IDX] == 'T') {
+            auto divisor_string = line.substr(TEST_DIVISOR_START);
             uint64_t divisor = 0;
-            for(auto c : divisor_string) {
-                divisor = divisor * 10 + c - '0';
+            for(auto c : divisor_string) {  // NOLINT(readability-identifier-length)
+                divisor = divisor * 10 + c - '0';  // NOLINT(*magic-numbers)
             }
             monkeys[monkey_id].test = std::function {
                 [divisor](uint64_t worry_level) {
                     return worry_level % divisor == 0;
                 }
             };
-        } else if(line[7] == 't') {
-            monkeys[monkey_id].true_destination = line[29] - '0';
+        } else if(line[LINE_TRUE_DEST_IDX] == 't') {
+            monkeys[monkey_id].true_destination = line[TRUE_DEST_IDX] - '0';
         } else {
-            monkeys[monkey_id].false_destination = line[30] - '0';
+            monkeys[monkey_id].false_destination = line[FALSE_DEST_IDX] - '0';
         }
     }
-    for(int round = 0; round < 20; round++) {
+    for(int round = 0; round < N_ROUNDS_1; round++) {
         for(size_t i = 0; i < N_MONKEYS; i++) {
             auto monkey = &(monkeys[i]);
             monkey->n_inspections += monkey->worry_levels.size();
             for(auto level : monkey->worry_levels) {
-                auto new_level = monkey->operation(level) / 3;
+                auto new_level = monkey->operation(level) / WORRY_DIVISOR;
                 auto destination = monkey->test(new_level) ? monkey->true_destination : monkey->false_destination;
                 monkeys[destination].worry_levels.push_back(new_level);    
             }
@@ -161,25 +178,25 @@ uint64_t part2(const char* path) {
         if(line.length() == 0) {
             continue;
         }
-        if(line[0] == 'M') {
-            monkey_id = line[7] - '0';
-        } else if(line[2] == 'S') {
-            auto items_string = line.substr(18);
+        if(line[LINE_MONKEY_ID_IDX] == 'M') {
+            monkey_id = line[MONKEY_ID_IDX] - '0';
+        } else if(line[LINE_STARTING_LEVELS_IDX] == 'S') {
+            auto items_string = line.substr(ITEMS_START);
             uint64_t worry_level = 0;
-            for(auto c : items_string) {
+            for(auto c : items_string) {  // NOLINT(readability-identifier-length)
                 if(c == ',') {
                     monkeys[monkey_id].worry_levels.push_back(worry_level);
                     worry_level = 0;
                 } else if(c == ' ') {
                     continue;
                 } else {
-                    worry_level = worry_level * 10 + c - '0';
+                    worry_level = worry_level * 10 + c - '0';  // NOLINT(*magic-numbers)
                 }
             }
             monkeys[monkey_id].worry_levels.push_back(worry_level);
-        } else if(line[2] == 'O') {
-            auto operation_type = line[23];
-            auto operation_value_string = line.substr(25);
+        } else if(line[LINE_OPERATION_IDX] == 'O') {
+            auto operation_type = line[OPERATION_TYPE_IDX];
+            auto operation_value_string = line.substr(OPERATION_VAL_START);
             if(operation_value_string[0] == 'o') {
                 if(operation_type == '*') {
                     monkeys[monkey_id].operation = std::function {
@@ -196,8 +213,8 @@ uint64_t part2(const char* path) {
                 }
             } else {
                 uint64_t operation_value = 0;
-                for(auto c : operation_value_string) {
-                    operation_value = operation_value * 10 + c - '0';
+                for(auto c : operation_value_string) {  // NOLINT(readability-identifier-length)
+                    operation_value = operation_value * 10 + c - '0';  // NOLINT(*magic-numbers)
                 }
                 if(operation_type == '*') {
                     monkeys[monkey_id].operation = std::function {
@@ -213,11 +230,11 @@ uint64_t part2(const char* path) {
                     };
                 }
             }
-        } else if(line[2] == 'T') {
-            auto divisor_string = line.substr(21);
+        } else if(line[LINE_TEST_IDX] == 'T') {
+            auto divisor_string = line.substr(TEST_DIVISOR_START);
             uint64_t divisor = 0;
-            for(auto c : divisor_string) {
-                divisor = divisor * 10 + c - '0';
+            for(auto c : divisor_string) {  // NOLINT(readability-identifier-length)
+                divisor = divisor * 10 + c - '0';  // NOLINT(*magic-numbers)
             }
             common_test_divisor *= divisor;
             monkeys[monkey_id].test = std::function {
@@ -225,13 +242,13 @@ uint64_t part2(const char* path) {
                     return worry_level % divisor == 0;
                 }
             };
-        } else if(line[7] == 't') {
-            monkeys[monkey_id].true_destination = line[29] - '0';
+        } else if(line[LINE_TRUE_DEST_IDX] == 't') {
+            monkeys[monkey_id].true_destination = line[TRUE_DEST_IDX] - '0';
         } else {
-            monkeys[monkey_id].false_destination = line[30] - '0';
+            monkeys[monkey_id].false_destination = line[FALSE_DEST_IDX] - '0';
         }
     }
-    for(int round = 0; round < 10000; round++) {
+    for(int round = 0; round < N_ROUNDS_2; round++) {
         for(size_t i = 0; i < N_MONKEYS; i++) {
             auto monkey = &(monkeys[i]);
             monkey->n_inspections += monkey->worry_levels.size();
